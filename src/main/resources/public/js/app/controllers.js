225c8c4abb7a8c508fe84f17e85a0de1ce3df799
caozhen.ai <caozhen.ai@qq.com>
2019-04-05 17:38:00 +0800

    JS CDN

--- src/main/resources/public/js/app/controllers.js
+++ src/main/resources/public/js/app/controllers.js
@@ -230,6 +230,17 @@ function HomeCtrl($scope, $http, $state, $filter) {
 
     $scope.today = $filter('date')(new Date(), 'yyyy-MM-dd');
 
+    // 点赞
+    $scope.upvote = function (toMemberId) {
+
+        $http({
+            url: '/api/upvote/webVote',
+            method: 'post',
+            params: {'fromMemberId': -1, 'toMemberId': toMemberId}
+        }).success(function (response) {
+            console.log("upvote: " + toMemberId);
+        })
+    };
 
     $(document).ready(function () {
         $('#dataTable').DataTable({
@@ -242,6 +253,14 @@ function HomeCtrl($scope, $http, $state, $filter) {
             "order": [[3, 'desc'], [4, 'desc']],
             "deferRender": true,
             "columns": [
+                {
+                    "data": "",
+                    "title": "排名",
+                    "width": "50px",
+                    render: function (data, type, row, meta) {
+                        return meta.row + 1;
+                    }
+                },
                 {
                     "data": "username",
                     "title": "用户名",
@@ -274,18 +293,17 @@ function HomeCtrl($scope, $http, $state, $filter) {
                 {
                     "data": "solvedProblemNumberOfToday",
                     "title": "今日刷题",
-                    "width": "100px",
+                    "width": "50px"
                 },
                 {
                     "data": "solvedQuestion",
                     "title": "刷题数",
-                    "width": "100px",
+                    "width": "50px"
                 },
                 {
                     "data": "upvoteNumber",
                     "title": "点赞",
-                    "width": "100px",
-
+                    "width": "50px"
                 }
             ]
         })
@@ -325,149 +343,7 @@ function AddUpvoteCount(index) {
 }
 
 
-function CheckRatioCtrl1($scope, $http, $state, $filter) {
-
-    $scope.days = null;
-    $scope.ratioList = null;
-
-    $scope.today = $filter('date')(new Date(), 'yyyy-MM-dd');
-
-    $http({
-        url: 'checkDayInfo/checkRatioList',
-        method: 'get',
-        params: {date: $scope.today}
-    }).success(function (response) {
-
-        if (response != null) {
-            $scope.days = response.days;
-            $scope.ratioList = response.ratioList;
-
-
-        }
-    });
-
-// Set new default font family and font color to mimic Bootstrap's default styling
-    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-    Chart.defaults.global.defaultFontColor = '#858796';
-
-    function number_format(number, decimals, dec_point, thousands_sep) {
-        // *     example: number_format(1234.56, 2, ',', ' ');
-        // *     return: '1 234,56'
-        number = (number + '').replace(',', '').replace(' ', '');
-        var n = !isFinite(+number) ? 0 : +number,
-            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-            s = '',
-            toFixedFix = function (n, prec) {
-                var k = Math.pow(10, prec);
-                return '' + Math.round(n * k) / k;
-            };
-        // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-        if (s[0].length > 3) {
-            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-        }
-        if ((s[1] || '').length < prec) {
-            s[1] = s[1] || '';
-            s[1] += new Array(prec - s[1].length + 1).join('0');
-        }
-        return s.join(dec);
-    }
-
-// Area Chart Example
-    var ctx = document.getElementById("myAreaChart");
-    var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: $scope.days,
-            datasets: [{
-                label: "Earnings",
-                lineTension: 0.3,
-                backgroundColor: "rgba(78, 115, 223, 0.05)",
-                borderColor: "rgba(78, 115, 223, 1)",
-                pointRadius: 3,
-                pointBackgroundColor: "rgba(78, 115, 223, 1)",
-                pointBorderColor: "rgba(78, 115, 223, 1)",
-                pointHoverRadius: 3,
-                pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-                pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-                pointHitRadius: 10,
-                pointBorderWidth: 2,
-                data: $scope.ratioList,
-            }],
-        },
-        options: {
-            maintainAspectRatio: false,
-            layout: {
-                padding: {
-                    left: 10,
-                    right: 25,
-                    top: 25,
-                    bottom: 0
-                }
-            },
-            scales: {
-                xAxes: [{
-                    time: {
-                        unit: 'date'
-                    },
-                    gridLines: {
-                        display: false,
-                        drawBorder: false
-                    },
-                    ticks: {
-                        maxTicksLimit: 7
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        maxTicksLimit: 5,
-                        padding: 10,
-                        // Include a dollar sign in the ticks
-                        callback: function (value, index, values) {
-                            return '$' + number_format(value);
-                        }
-                    },
-                    gridLines: {
-                        color: "rgb(234, 236, 244)",
-                        zeroLineColor: "rgb(234, 236, 244)",
-                        drawBorder: false,
-                        borderDash: [2],
-                        zeroLineBorderDash: [2]
-                    }
-                }],
-            },
-            legend: {
-                display: false
-            },
-            tooltips: {
-                backgroundColor: "rgb(255,255,255)",
-                bodyFontColor: "#858796",
-                titleMarginBottom: 10,
-                titleFontColor: '#6e707e',
-                titleFontSize: 14,
-                borderColor: '#dddfeb',
-                borderWidth: 1,
-                xPadding: 15,
-                yPadding: 15,
-                displayColors: false,
-                intersect: false,
-                mode: 'index',
-                caretPadding: 10,
-                callbacks: {
-                    label: function (tooltipItem, chart) {
-                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                        return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-                    }
-                }
-            }
-        }
-    });
-
-}
-
-
+// 小组统计数据
 function CheckRatioCtrl($scope, $http, $state, $filter) {
 
     $scope.days = null;
